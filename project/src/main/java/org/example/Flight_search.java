438962fbd30db334d4a063bd3ef5f4867eab8d64
tanish1409 <tanishsingla02@gmail.com>
2024-03-15 23:37:22 -0400

    Remove non-parameterized test

--- project/src/main/java/org/example/Flight_search.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Flight_search {
-    public static boolean inputValidation(String[] arr){
-        return true;
-    }
-}
--- project/src/main/java/org/example/InputValidator.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class InputValidator {
-    public boolean validateInput(String[] input) {
-        // Your validation logic here
-        return true; // Placeholder return value
-    }
-}
