7e0524379618855bf58f706dca25b2596fdae82a
Syed Nasir <snasirh09@gmail.com>
2024-03-15 23:58:18 -0400

    Updated mock flight_search

--- finalProject/src/main/java/com/ontariotechu/sofe3980U/Flight_search.java
+++ finalProject/src/main/java/com/ontariotechu/sofe3980U/Flight_search.java
@@ -1,4 +1,7 @@
+
 package com.ontariotechu.sofe3980U;
+
+import java.util.List;
 
 public class Flight_search {
     public boolean inputValidation(String startLoc, String endLoc, String startDate, String endDate, String startTime, String endTime){
@@ -7,5 +10,11 @@ public class Flight_search {
 
     public String[][] timeFormat24(String[][] input) throws IllegalArgumentException{
         return new String[0][];
+    }
+
+     public List<Flight> getFlights(String from, String to, String date, String startTime, String endTime) {
+        // Dummy implementation
+        // Return an example list of flights or mock data
+        return List.of(new Flight(from, to, date, startTime, endTime));
     }
 }
