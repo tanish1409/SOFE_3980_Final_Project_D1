5ba9947f8e7c7122d755715d71d587515bcadc72
tanish1409 <tanishsingla02@gmail.com>
2024-04-02 00:46:26 -0400

    Final Try Push

--- finalProject/src/main/java/com/ontariotechu/sofe3980U/FlightBookingPageController.java
+++ finalProject/src/main/java/com/ontariotechu/sofe3980U/FlightBookingPageController.java
@@ -1,11 +1,28 @@
 package com.ontariotechu.sofe3980U;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FlightBookingPageController {
+
+    Flight_search flightSearch = new Flight_search();
+    One_Way oneWay = new One_Way();
+    //    @Autowired
+//    private One_Way oneWay;
+//
+//    @Autowired
+//    private Round_Trip roundTrip;
+//
+//    @Autowired
+//    private Flight_Search flightSearch = new Flight_Search();
+//
+//    @Autowired
+//    private Ticket_Generate ticketGenerate;
 
     @GetMapping("/")
     public String showFlightBookingPage() {
@@ -13,8 +30,19 @@ public class FlightBookingPageController {
     }
 
     @PostMapping("/search")
-    public String search() {
+    public String searchFlights(@RequestParam("flightType") String flightType,
+                                @RequestParam("from") String from,
+                                @RequestParam("to") String to,
+                                Model model) {
+        String[][] flights = null;
+        flightSearch.queryDB(from, to);
+        flights = flightSearch.getFlightOptions();
+//        if ("oneWay".equals(flightType)) {
+//            flights = oneWay.searchFlights(from, to);
+//        } else if ("roundTrip".equals(flightType)) {
+//            flights = roundTrip.searchFlights(from, to);
+//        }
+        model.addAttribute("flights", flights);
         return "flight_results";
     }
 }
-// Path: src/main/resources/templates/flight_booking.html
